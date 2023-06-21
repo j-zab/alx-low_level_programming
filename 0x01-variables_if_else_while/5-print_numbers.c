075bd7965dd4bb89349c9b4b6de480a93916cf22
job zablon <zablonjob@gmail.com>
2023-06-21 00:20:25 -0700

    prints all single digit number of base 10 starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * main - main block
- * prints all single digit number of base 10 starting from 0
+ * prints all single digit numbers of base 10 starting from 0
  * Return: 0
 */
 
@@ -10,7 +10,7 @@ int main(void)
 {
 int i;
 
-for (i = 0: i <10; i++)
+for (i = 0; i < 10; i++)
 putchar('0' + i);
 
 putchar('\n');

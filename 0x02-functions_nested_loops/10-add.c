e7d3ce15d0d4bbb401bec9c5244ff85fd8b9d107
job zablon <zablonjob@gmail.com>
2023-06-22 15:48:49 -0700

    a + b updated

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - main block
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *  function that prints the sign of a number.
+ * Return: always 1
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

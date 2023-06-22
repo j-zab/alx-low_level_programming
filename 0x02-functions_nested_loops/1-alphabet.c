0a1d7179f4d3eadb1322e15a005bd818e995e243
job zablon <zablonjob@gmail.com>
2023-06-22 14:17:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * return : returns 0
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	_putchar(alph);
+	_putchar('\n');
+}

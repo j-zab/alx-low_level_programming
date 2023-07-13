5a7e5c78f271a9320bdd4bfcd85bf4f336f96487
job zablon <zablonjob@gmail.com>
2023-07-13 03:54:23 -0700

    Static libraries

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-int _putchar(char c)
+int _islower(int c)
 {
-	return (write(1, &c, 1));
+	return (c >= 'a' && c <= 'z');
 }

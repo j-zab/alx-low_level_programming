e80a2a4bc2ad59db21ca23c6204eb8a06445997d
job zablon <zablonjob@gmail.com>
2023-07-31 21:31:17 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,18 @@
  *
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
-	while (h)
+
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
 	}
 
-	return (num);
+	return (count);
 }

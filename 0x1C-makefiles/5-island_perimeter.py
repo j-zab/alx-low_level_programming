f6d96377cb8bc1cfdf91288ec24da90f05df9a89
j-zab <zablonjob@yahoo.com>
2024-03-22 10:26:07 +0000

    make files correction

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,39 @@
- the perimeter of an island  measuring function."""
+ #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in the grid.
-    Args:
-        grid (list): A list of list of integers representing an island
-    Returns:
-        The perimieter of the island."""
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    """
+Constraints:
+    - 0 represents a water zone, 1 represents a land zone.
+    - One cell is a square with a side length of 1.
+    - Grid cells are connected horizontally/vertically (not diagonally).
+    - Grid is rectangular, with width and height not exceeding 100.
+    - Grid is completely surrounded by water,
+        with only one island (or nothing).
+    - The island doesn’t have “lakes”
+        (water inside that isn’t connected to the water around the island).
+    """
 
-    for i in range(height):
-        for j in range(width):
+    rows = len(grid)
+    cols = len(grid[0]) if rows else 0
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
